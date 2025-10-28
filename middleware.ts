091d90637d7cd7916c7672b71ab21a21ac09c1f9
coldlavaai <otatler@gmail.com>
2025-10-28 20:51:36 +0000

    Fix middleware to allow public access to images and logos

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,7 @@ import { authConfigEdge } from './auth.config.edge'
 export default NextAuth(authConfigEdge).auth
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|public).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|favicon.png|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.svg|logos).*)'
+  ],
 }
